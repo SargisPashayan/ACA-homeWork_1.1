104d29db35c349d054e61cb1b629db4c9e20f118
SargisPashayan <pashayansargisl@gmail.com>
2021-04-28 22:43:24 +0400

    logicOp file changed

--- homework1.2/logicOperator/logicOp.js
+++ homework1.2/logicOperator/logicOp.js
@@ -5,6 +5,27 @@ let age;
 if(age >= 14 && age <= 94); // age 
 
 
-if (-1 || 0) alert( 'first' ); // print(boolean of -1 is true)
-if (-1 && 0) alert( 'second' );
-if (null || -1 && 1) alert( 'third' );
+if (-1 || 0) alert('first'); // print(boolean of -1 is true)
+if (-1 && 0) alert('second');
+if (null || -1 && 1) alert('third');
+
+
+let user = prompt("Who's there?", "");
+
+if (user === "Admin") {
+  let pass = prompt("Password?", "");
+
+  if (pass === "TheMaster") {
+    alert("Welcome!");
+  } else if (pass === "" || pass === null) {
+    alert("Canceled");
+  } else {
+    alert("Wrong password");
+  }
+} else if (user === "" || user === null) {
+  alert("Canceled");
+} else {
+  alert("I don't know you");
+}
+
+
