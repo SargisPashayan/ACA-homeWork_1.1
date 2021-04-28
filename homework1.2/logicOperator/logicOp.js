e1fba2e0c089104e1b618296482d57b450cbd176
SargisPashayan <pashayansargisl@gmail.com>
2021-04-28 22:19:19 +0400

    file with logic operators added

---
+++ homework1.2/logicOperator/logicOp.js
@@ -0,0 +1,10 @@
+alert( alert(1) && alert(2) ); // alert(1) print 1 and return value undefined then global alert print undefined
+
+
+let age;
+if(age >= 14 && age <= 94); // age 
+
+
+if (-1 || 0) alert( 'first' ); // print(boolean of -1 is true)
+if (-1 && 0) alert( 'second' );
+if (null || -1 && 1) alert( 'third' );
