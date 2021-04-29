6fba0db13a77d0a519d57621c9e816a4d22577e6
SargisPashayan <pashayansargisl@gmail.com>
2021-04-30 00:34:11 +0400

    task10 modified

--- homework1.4/task10.js
+++ homework1.4/task10.js
@@ -1,23 +1,23 @@
 
 //#10
-let num = 5;
+let num = 20;
 let fib0 = 0;
 let fib1 = 1;
 let fibNum = 0;
 
 if (num === 0) {
     console.log(fib0);
+    throw new Error('This is not an error. This is just to abort javascript');
 }
 
-else if (num === 1) {
+if (num === 1) {
     console.log(fib1);
+    throw new Error('This is not an error. This is just to abort javascript');
 }
  
-else {
-    for (let i = 2; i < num; i++) {
-        finNum = fib0 + fib1;
-        fib0 = fib1;
-        fib1 = fibNum;
-    }
-    console.log(fibNum);
+for (let i = 2; i <= num; i++) {
+    fibNum = fib0 + fib1;
+    fib0 = fib1;
+    fib1 = fibNum;
 }
+console.log(fibNum);
