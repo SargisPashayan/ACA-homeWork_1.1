16c80a8c724daef4011f806f029555c70619f48c
SargisPashayan <pashayansargisl@gmail.com>
2021-04-30 16:26:31 +0400

    task modified

---
+++ homework1.4/task20.js
@@ -0,0 +1,17 @@
+
+    let rows = 5;
+    let output = '';
+    for (let i = 1; i <= rows; i++) {
+      let n = 0;
+      let c = 0;
+      let out = i;
+      for (let j = 1; j <= i; j++) {
+        c = out + n; 
+        output += c + ",";
+        out = c;
+        n = rows - j;  
+      }
+      rows = 5;
+      console.log(output);
+      output = "";
+    }
