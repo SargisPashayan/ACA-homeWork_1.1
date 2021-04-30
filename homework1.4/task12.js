2db5dc6500db4b705a60ffb590462ced31447917
SargisPashayan <pashayansargisl@gmail.com>
2021-04-30 11:03:48 +0400

    task added

---
+++ homework1.4/task12.js
@@ -0,0 +1,12 @@
+let sentence = "_ we have a, _";
+let arr = ["Huston", "lord"];
+let str = sentence.split(" ");
+for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        if(str[i] === "_"){
+            str[i] = arr[j];
+            arr.shift();
+        }
+    }
+}
+console.log(str.join(" "));
