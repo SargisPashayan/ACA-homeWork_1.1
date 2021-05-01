77c11480913233a8078a1b77308071ddaa1634ea
SargisPashayan <pashayansargisl@gmail.com>
2021-05-01 16:43:16 +0400

    task 12 modifyed

--- homework1.4/task12.js
+++ homework1.4/task12.js
@@ -1,10 +1,14 @@
-let sentence = "_ we have a, _";
+let sentence = "_, we have a _";
 let arr = ["Huston", "lord"];
 let str = sentence.split(" ");
 for (let i = 0; i < str.length; i++) {
     for (let j = 0; j < arr.length; j++) {
         if(str[i] === "_"){
             str[i] = arr[j];
+            arr.shift();
+        }
+        else if(str[i] === "_,") {
+            str[i] = arr[j] + ",";
             arr.shift();
         }
     }
