ee05b43f1ea7523f7d319d721a3a246d2616dff9
SargisPashayan <pashayansargisl@gmail.com>
2021-05-06 00:45:54 +0400

    feat: seveth task added

---
+++ homework1.6/threeChars7.js
@@ -0,0 +1,14 @@
+let str = "aweyoolp";
+
+let charMoving = function (str) {
+
+    let arr = str.split("");
+    for (let i = 0; i < arr.length - 2; i += 3) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = arr[i + 2];
+        arr[i + 2] = temp;
+    }
+    return arr.join("");
+}
+console.log(charMoving(str));
