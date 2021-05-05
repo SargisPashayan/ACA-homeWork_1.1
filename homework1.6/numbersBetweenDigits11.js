4b903e90227ced1c2bc729a755c20fd315839f6f
SargisPashayan <pashayansargisl@gmail.com>
2021-05-05 23:24:01 +0400

    feat: Numbers in define interval

---
+++ homework1.6/numbersBetweenDigits11.js
@@ -0,0 +1,32 @@
+let inputMin = 99;
+let inputMax = 199;
+
+
+let findIntervalNums = function(numMin, numMax) {
+    let arr = [];
+    let res = [];
+    for(i = numMin; i <= numMax; i++) {
+        if(i % 2 === 0){
+            arr.push(i);
+        }
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+
+        arr[i] = arr[i].toString();
+        
+        for(let j = 0; j < arr[i].length; j++) {
+            if(arr[i][j] % 2 === 0 && arr[i][j + 1] % 2 === 0) {
+                res.push(parseInt(arr[i]));
+            }else {
+                break;
+            }
+        }
+    }
+    if(res.length === 0) {
+        return "Such numbers does not exist."
+    }else {
+        return res;
+    }
+}
+console.log(findIntervalNums(inputMin, inputMax));
