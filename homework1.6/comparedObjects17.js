7edadddc2aa5ed4b7534d3ab8a538bcc5e0c1460
SargisPashayan <pashayansargisl@gmail.com>
2021-05-07 18:08:20 +0400

    fix: task was modified

--- homework1.6/comparedObjects17.js
+++ homework1.6/comparedObjects17.js
@@ -2,8 +2,8 @@ let a = {a: 1};
 let b = {a: 1, b: 2};
 
 function shallowCompare(obj1, obj2) {
-  let keys1 = Object.keys(obj1);
-  let keys2 = Object.keys(obj2);
+  let keys1 = Object.keys(obj1).sort();
+  let keys2 = Object.keys(obj2).sort();
 
   if (keys1.length !== keys2.length) {
       console.log(false);
