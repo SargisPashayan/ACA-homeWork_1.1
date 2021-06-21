07dce921760fee17452a469e7d5375705579b0ab
SargisPashayan <pashayansargisl@gmail.com>
2021-06-22 01:23:53 +0400

    fix: html and css files modyfied

---
+++ homework1.21/timer.js
@@ -0,0 +1,8 @@
+let getMin = document.getElementById('min');
+let getSec = document.getElementById('sec');
+let getMilisec = document.getElementById('milisec');
+
+let min = 0;
+let sec = 0;
+let milisec = 0;
+
